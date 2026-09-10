00012582e343a6c0aa208752852fc0f3f8ad9071
dylan <dylanb.connect@gmail.com>
2026-09-10 10:12:50 -0700

    Added logic to get the seed URL from the user. I used a Scanner and I might change this in the future.

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] theArgs) {
+        // I want to develop a web crawler that has multithreading, PostgreSQL, indexing/searching, Spring Boot API, and Docker support.
+
+        // I dont know where to begin.
+
+        // Give my Java program a URL, download the webpage, and print its contents.
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.print("Please enter the URL: ");
+        String userInput = sc.nextLine();
+
+
+        sc.close();
+    }
+}
+
