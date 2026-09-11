d77bb975d7ff330451e58cd6c2c9afa08d4d65d5
dylan <dylanb.connect@gmail.com>
2026-09-11 14:05:03 -0700

    Added getTag method to retreive the HTML tag (the text in between < > such as <p></p>)

--- HtmlParser.java
+++ HtmlParser.java
@@ -4,8 +4,41 @@ public class HtmlParser {
 
     }
 
-    public void parse(String theHtml) {
+    public void parse(final String theHtml) {
+        int currentIndex = 0;
+
+        // Traverse the entire HTML
+        while (!currentIndex.equals(theHtml.length())) {
+
+            char currentChar = theHtml.charAt(currentIndex);
+
+            // Parse the tag
+            if (currentChar == '<') {
+                
+                String tag = getTag(currentIndex + 1, theHtml);
+                currentIndex += tag.length() + 2; // +2 for the '<' and '>'
+
+            // Parse the text
+            } else {
+
+            }
+            currentIndex++;
+        }
+    }
+
+    // Extracts the tag from the HTML starting at the given index
+    public String getTag(int theCurrentIndex, final String theHtml) {
+        StringBuilder tag = new StringBuilder();
+        char theCurrentChar = theHtml.charAt(theCurrentIndex);
+
+        // Traverse until the end of the tag
+        while (theCurrentChar != '>') {
+            tag.append(theCurrentChar);
+            theCurrentIndex++;
+            theCurrentChar = theHtml.charAt(theCurrentIndex);
+        }
         
+        return tag.toString();
     }
 
 
