f2dba31e602e8a7e1a6691db157f74bce71604e6
dylan <dylanb.connect@gmail.com>
2026-09-11 17:44:50 -0700

    Added getBody() method that extracts the body text in between the HTML tags. I now can parse HTML by extracting tags and body text. I also added checks at the beginning of these methods in the case that the HTML I am parsing is incomplete

--- HtmlParser.java
+++ HtmlParser.java
@@ -6,23 +6,27 @@ public class HtmlParser {
 
     public void parse(final String theHtml) {
         int currentIndex = 0;
-
+        String body = "";
         // Traverse the entire HTML
-        while (currentIndex != theHtml.length()) {
-
+        while (currentIndex < theHtml.length()) {
+            //System.out.println("Current index: " + currentIndex);
+            //System.out.println("Current char: " + theHtml.charAt(currentIndex));
             char currentChar = theHtml.charAt(currentIndex);
 
             // Parse the tag
             if (currentChar == '<') {
                 String tag = getTag(currentIndex + 1, theHtml); // (currentIndex + 1) to get past the starting '<'
                 System.out.println("Tag: " + tag);
-                currentIndex += tag.length() + 1; // (tag.length() + 1) to get past the ending '>'
+                currentIndex += tag.length() + 2; // (tag.length() + 2) to account for '<' and '>'
 
             // Parse the text
             } else {
-
+                body = getBody(currentIndex, theHtml);
+                System.out.println("Body: " + body);
+                currentIndex += body.length();
+                
             }
-            currentIndex++;
+            //currentIndex++;
         }
     }
 
@@ -33,12 +37,32 @@ public class HtmlParser {
 
         // Traverse until the end of the tag
         while (theCurrentChar != '>') {
+            if (theCurrentIndex == theHtml.length() - 1) {
+                throw new IllegalArgumentException("HTML tag is missing closing '>'");
+            }
             tag.append(theCurrentChar);
             theCurrentIndex++;
             theCurrentChar = theHtml.charAt(theCurrentIndex);
         }
         
         return tag.toString();
+    }
+
+    public String getBody(int theCurrentIndex, final String theHtml) {
+        StringBuilder body = new StringBuilder();
+        char theCurrentChar = theHtml.charAt(theCurrentIndex);
+
+        // Traverse until the beginning of the next tag
+        while (theCurrentChar != '<') {
+            if (theCurrentIndex == theHtml.length() - 1) {
+                throw new IllegalArgumentException("HTML text body has no ending point");
+            }
+            body.append(theCurrentChar);
+            theCurrentIndex++;
+            theCurrentChar = theHtml.charAt(theCurrentIndex);
+        }
+        
+        return body.toString();
     }
 
 
