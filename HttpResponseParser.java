5f588385f7516b1ae2a87eedacce09b7abdeaedc
dylan <dylanb.connect@gmail.com>
2026-09-13 15:29:12 -0700

    Added 3 methods to be implemented; getStatus() - returns status code, getHeaders() - returns the raw response headers, and getHtml() - returns the actual HTML that is needed to be parsed by HtmlParser.java

--- HttpResponseParser.java
+++ HttpResponseParser.java
@@ -1,4 +1,20 @@
 public class HttpResponseParser {
 
     
+    public HttpResponseParser() {
+
+    }
+
+    public String getStatus(String theResponse) {
+
+    }
+
+    public String getHeaders(String theResponse) {
+        
+    }
+
+    public String getHtml(String theResponse) {
+        
+    }
+
 }
