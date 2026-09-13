ed1cc9629697f86172a06ac3a7bde7269200fe67
dylan <dylanb.connect@gmail.com>
2026-09-13 16:21:43 -0700

    Implemented getStatus() method and created vadility checks to make sure the status code exists and that its within the valid range of HTTP status codes

--- HttpResponseParser.java
+++ HttpResponseParser.java
@@ -5,16 +5,51 @@ public class HttpResponseParser {
 
     }
 
-    public String getStatus(String theResponse) {
+    public int getStatus(String theResponse) {
+        int currentIndex = 0;
+        StringBuilder result = new StringBuilder();
 
+        // Keep incrementing until the first whitespace is encountered.
+        while (currentIndex < theResponse.length() && theResponse.charAt(currentIndex) != ' ') {
+            currentIndex++;
+        }
+
+        // Skip any whitespaces between protocol and status code
+        while (currentIndex < theResponse.length() && theResponse.charAt(currentIndex) == ' ') {
+            currentIndex++;
+        }
+
+        // Collect digits until the next whitespace
+        if (Character.isDigit(theResponse.charAt(currentIndex))) {
+            while (currentIndex < theResponse.length() && theResponse.charAt(currentIndex) != ' ') {
+                result.append(theResponse.charAt(currentIndex));
+                currentIndex++;
+            }
+        }
+
+        if (result.length() == 0) {
+            throw new IllegalArgumentException("No status code found.");
+        }
+
+        int statusCode = Integer.parseInt(result.toString());
+
+        if (!isValidHttpStatusCode(statusCode)) {
+            throw new IllegalArgumentException("Not a valid status code: " + statusCode);
+        }
+
+        return statusCode;
     }
 
     public String getHeaders(String theResponse) {
-        
+        return "";
     }
 
     public String getHtml(String theResponse) {
-        
+        return "";
+    }
+
+    public boolean isValidHttpStatusCode(final int theStatusCode) {
+        return theStatusCode >= 100 && theStatusCode <= 599;
     }
 
 }
