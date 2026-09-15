a70829c8800ff700150c03221d22d304f030d2ad
dylan <dylanb.connect@gmail.com>
2026-09-14 20:42:50 -0700

    Added logic for the method getHeaders() which successfully returns the headers. There are prob some edge cases I need to go back and handle but it works!

--- HttpResponseParser.java
+++ HttpResponseParser.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class HttpResponseParser {
 
     
@@ -38,8 +41,53 @@ public class HttpResponseParser {
         return statusCode;
     }
 
-    public String getHeaders(String theResponse) {
-        return "";
+    public HashMap<String, String> getHeaders(String theResponse) {
+        HashMap<String, String> headers = new HashMap<>();
+        int currentIndex = 0;
+
+        while (currentIndex < theResponse.length()) {
+            // Date: Mon, 14 Sep 2026 20:08:22 GMT\r\nContent-Type: text/html\r\n\r\n
+
+            StringBuilder name = new StringBuilder(); // Reset or instantiate the name var
+            StringBuilder value = new StringBuilder(); // Reset or instantiate the value var
+
+
+            // Return if the end is reached
+            if ((theResponse.length() - currentIndex) >= 2
+                && theResponse.substring(currentIndex, currentIndex + 2).equals("\r\n")) {
+                
+                return headers;
+            }
+
+            // Add the current character to *name* if it is before the colon
+            while (currentIndex < theResponse.length() && theResponse.charAt(currentIndex) != ':') {
+                name.append(theResponse.charAt(currentIndex));
+                currentIndex++;
+            }
+
+            if (currentIndex < theResponse.length() && theResponse.charAt(currentIndex) == ':') {
+                currentIndex++; // Accounts for the colon ':'
+            }
+
+            // Add the current character to *value* if it is before the string "\r\n"
+            while ((theResponse.length() - currentIndex) >= 2
+                && !theResponse.substring(currentIndex, currentIndex + 2).equals("\r\n")) {
+
+                value.append(theResponse.charAt(currentIndex));
+                currentIndex++;
+            }
+
+            // Make sure we are not at the end of the string and that the next two characters are in fact 'r' and 'n'
+            if ((theResponse.length() - currentIndex) >= 2 
+                && theResponse.substring(currentIndex, currentIndex + 2).equals("\r\n")) {
+
+                currentIndex += 2; // Accounts for the last string "\r\n"
+            }
+
+            headers.put(name.toString(), value.toString()); // Store the name and value info gathered
+        }
+
+        return headers;
     }
 
     public String getHtml(String theResponse) {
